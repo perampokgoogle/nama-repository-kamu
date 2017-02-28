93cecee21b56bfb131f485abb8f29f60b666a41e
perampokgoogle <localhost367@gmail.com>
2017-02-28 08:21:59 +0700

    Add files via upload

---
+++ Auto ALT Gambar Postingan.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[ 
+$(document).ready(function(){$("img").each(function(){var t=$(this),e=document.getElementById("imgus").innerHTML;t.attr("title",e),t.attr("alt",e)})});
+//]]>
+</script>
+
